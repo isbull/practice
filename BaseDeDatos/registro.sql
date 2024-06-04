9ef03863052f4e74a40510bcd0ce0714306d4770
Roberto Medrano <medranoisaul5@gmail.com>
2024-06-04 13:47:01 +0200

    practicandoNode

--- BaseDeDatos/registro.sql
+++ BaseDeDatos/registro.sql
@@ -10,9 +10,19 @@ CREATE TABLE IF NOT EXISTS usuario(
     pass VARCHAR (8) UNSIGNED NOT NULL,
     email VARCHAR (100) UNSIGNED NOT NULL,
     addre VARCHAR (250) NOT NULL,
-    age DATE 
+    age DATE,
+   
 )
 
 CREATE TABLE IF NOT EXISTS account(
+    nick_name VARCHAR(80) UNSIGNED NOT NULL,
+    pass VARCHAR (8) UNSIGNED NOT NULL,
+    email VARCHAR (100) UNSIGNED NOT NULL,
+    FOREIGN KEY(nick_name) REFERENCE usuario (nick_name)
 
 )
+
+INSERT INTO usuario (nick_name, pass, email,addre,age) VALUES
+('Isbull_29', 'pass1234', 'medranoisaul5@gmail.com', 'Calle falsa #123', '34');
+
+SELECT * FROM usuario;
---
+++ Node/app.js
@@ -0,0 +1,8 @@
+import {sum, rest, mult, div} from './operation.js'
+
+console.log(sum(1,2));
+console.log(rest(1,2));
+console.log(mult(1,2));
+console.log(div(1,2));
+
+
